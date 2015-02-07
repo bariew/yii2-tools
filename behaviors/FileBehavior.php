b737a9df33be7d6e64e5fb7124c8318c29252bc5
bariew home <bariew@yandex.ru>
2015-02-08 00:23:21 +0400

    added behaviors subfolder

--- behaviors/FileBehavior.php
+++ behaviors/FileBehavior.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-namespace bariew\yii2Tools;
+namespace bariew\yii2Tools\behaviors;
 
 use yii\db\ActiveRecord;
 use yii\base\Behavior;
