016c2878186653f57fe224df5113e3e05c741157
Pavel Bariev <bariew@yandex.ru>
2015-08-05 19:05:01 +0400

    added form helper

---
+++ helpers/FormHelper.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pt
+ * Date: 05.08.15
+ * Time: 17:25
+ */
+
+namespace bariew\yii2Tools\helpers;
+use yii\base\Model;
+use yii\db\ActiveRecord;
+
+
+class FormHelper
+{
+    public static function loadRelation(ActiveRecord $model, $relationName, $data)
+    {
+        /** @var ActiveRecord $relationClass */
+        $relationClass = $model->getRelation($relationName)->modelClass;
+
+        return static::loadMultiple(
+            @$data[(new $relationClass())->formName()],
+            $relationClass,
+            $model->$relationName
+        );
+    }
+
+    public static function loadMultiple($data, $modelClass, $models = [])
+    {
+        $result = ['models' => $models, 'errors' => []];
+        if (!$data) {
+            return $result;
+        }
+        $lastIndex = count($models);
+        foreach ($data as $id => $attributes) {
+            $modelSearch = array_filter($models, function($v) use($id){return $v['id'] == $id;});
+            /** @var Model $model */
+            $model = reset($modelSearch) ? : new $modelClass();
+            $model->load($attributes, '');
+            $index = $modelSearch ? key($modelSearch) : $lastIndex++;
+            $result['models'][$index] = $model;
+            if (!$model->validate()) {
+                $result['errors'][$index] = $model->errors;
+            }
+        }
+        return $result;
+    }
+}
