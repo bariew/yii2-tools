f7ae622b4360264b45bd37b23ca4290e26387b3f
Pavel Bariev <bariew@yandex.ru>
2015-09-21 19:21:14 +0400

    added default list

--- validators/ListValidator.php
+++ validators/ListValidator.php
@@ -7,12 +7,19 @@ use yii\validators\Validator;
 class ListValidator extends Validator
 {
     /**
+     * Set it if do not want to involve default {$attribute}List() method
+     * @var array
+     */
+    public $list;
+
+    /**
      * @inheritdoc
      */
     public function validateAttribute($model, $attribute)
     {
         $method = Inflector::camelize(str_replace('_id', '', $attribute).'List');
-        $result = in_array($model->$attribute, array_keys($model->$method()));
+        $list = ($this->list === null) ? array_keys($model->$method()) : $this->list;
+        $result = in_array($model->$attribute, $list);
         if (!$result) {
             $this->addError($model, $attribute, ($this->message ? : \Yii::t('app', 'Forbidden value')));
         }
