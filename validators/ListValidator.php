15bc467f525dc394d6efed135f43c0286a08cc74
Pavel Bariev <bariew@yandex.ru>
2015-09-22 13:47:20 +0400

    added validator custom model

--- validators/ListValidator.php
+++ validators/ListValidator.php
@@ -1,6 +1,7 @@
 <?php
 namespace bariew\yii2Tools\validators;
 
+use yii\base\Exception;
 use yii\helpers\Inflector;
 use yii\validators\Validator;
 
@@ -13,13 +14,22 @@ class ListValidator extends Validator
     public $list;
 
     /**
+     * Model for getting list
+     * @var \yii\db\ActiveRecord
+     */
+    public $model;
+
+    /**
      * @inheritdoc
      */
     public function validateAttribute($model, $attribute)
     {
+        $this->model = $this->model ? : $model;
         $method = Inflector::camelize(str_replace('_id', '', $attribute).'List');
-        $list = ($this->list === null) ? array_keys($model->$method()) : $this->list;
-        $result = in_array($model->$attribute, $list);
+        $list = ($this->list === null) ? array_keys($this->model->$method()) : $this->list;
+        $result = is_array($model->$attribute)
+            ? !array_diff($model->$attribute, $list)
+            : in_array($model->$attribute, $list);
         if (!$result) {
             $this->addError($model, $attribute, ($this->message ? : \Yii::t('app', 'Forbidden value')));
         }
