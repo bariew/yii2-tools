2405c3a6b473243ecb094f048b82474e1199da70
Pavel Bariev <bariew@yandex.ru>
2015-12-22 20:43:28 +0400

    fix

--- yii2yii/ActiveQuery.php
+++ yii2yii/ActiveQuery.php
@@ -9,9 +9,11 @@
 namespace bariew\yii2Tools\yii2yii;
 
 
+use yii\db\Query;
+
 class ActiveQuery extends \yii\db\ActiveQuery
 {
-    public $condition, $order;
+    public $condition, $order, $together;
     public $scopes = [];
 
     private $replacements = [
@@ -19,14 +21,16 @@ class ActiveQuery extends \yii\db\ActiveQuery
         'condition' => 'where',
     ];
 
-    private function replaceAttributes()
+    /**
+     *
+     */
+    public function replaceAttributes()
     {
         $class = $this->modelClass;
-        $table = $class::tableName();
+        $model = new $class();
         foreach ($this->replacements as $old => $new) {
             $this->$new = $this->$old;
         }
-
         if ($this->orderBy) {
             $resultOrders = [];
             $orders = is_array($this->orderBy) ? $this->orderBy : explode(',', $this->orderBy);
@@ -37,12 +41,36 @@ class ActiveQuery extends \yii\db\ActiveQuery
             }
             $this->orderBy = $resultOrders;
         }
-        if (is_array($this->where)) {
-            array_walk_recursive($this->where, function(&$v) use($table) {
-                $v = str_replace('t.', $table.'.', $v);;
-            });
-        } else {
-            $this->where = str_replace('t.', $table.'.', $this->where);
+
+        if ($this->join && !is_array($this->join)) {
+            $this->join = [$this->join];
+        }
+        if ($this->with) {
+            $with = [];
+            foreach ($this->with as $name => $value) {
+                $relName = is_numeric($name) ? $value : $name;
+                if (preg_match('/Count/', $relName)) {
+                    continue;
+                }
+                if (preg_match('/\./', $relName)) {
+                    continue;
+                }                $with[$name] = $value;
+                if (!is_array($value)) {
+                    continue;
+                }
+                /** @var ActiveQuery $relation */
+                $relation = call_user_func([$model, "get{$name}"]);
+                if ($relation->via) {
+                    $relation = $relation->via[1];
+                }
+                /** @var ActiveQuery $relationQuery */
+                $with[$name] = function ($relationQuery) use ($relation, $value) {
+                    $newQuery = new ActiveQuery($relation->modelClass, $value);
+                    $newQuery->replaceAttributes();
+                    $newQuery::merge($relationQuery->via[1], $newQuery);
+                };
+            }
+            $this->with = $with;
         }
     }
 
@@ -62,8 +90,17 @@ class ActiveQuery extends \yii\db\ActiveQuery
         return static::merge($query, $this);
     }
 
-    public static function merge(\yii\db\ActiveQuery $to, \yii\db\ActiveQuery $from)
+    public static function merge(\yii\db\ActiveQuery $to, $from)
     {
+        if (!$from) {
+            return $to;
+        }
+        if (is_array($from)) {
+            $from = new Query($from);
+        }
+        $class = $to->modelClass;
+        $table = $class::tableName();
+        /** @var \yii\db\ActiveQuery $from */
         $to->andWhere($from->where);
         $to->addParams($from->params);
         foreach (['orderBy', 'limit', 'with'] as $param) {
@@ -71,11 +108,43 @@ class ActiveQuery extends \yii\db\ActiveQuery
                 $to->$param = $from->$param;
             }
         }
+        foreach (['join', 'where'] as $param) {
+            if ($to->where) {
+                static::replaceInArray($to->$param, function(&$v) use($table) {
+                    $v = preg_replace('/[\w_]+\./', $table.'.', $v);
+                });
+            }
+        }
+
         return $to;
     }
 
     public function mergeWith($query)
     {
         return static::merge($this, $query);
+    }
+
+    public static function replaceInArray(&$data, $function)
+    {
+        if (is_array($data)) {
+            array_walk_recursive($data, $function);
+            static::arrayWalkRecursiveKeys($data, $function);
+        } else {
+            call_user_func_array($function, [&$data]);
+        }
+    }
+
+    public static function arrayWalkRecursiveKeys(&$array, $function)
+    {
+        if (!is_array($array)) {
+            return;
+        }
+        $result = [];
+        foreach ($array as $k => $v) {
+            call_user_func_array($function, [&$k]);
+            static::arrayWalkRecursiveKeys($v, $function);
+            $result[$k] = $v;
+        }
+        $array = $result;
     }
 }
