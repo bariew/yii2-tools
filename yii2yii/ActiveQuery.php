5bb3d84462d2c882dc2f59889762597694fcd3cf
Pavel Bariev <bariew@yandex.ru>
2015-12-24 10:23:01 +0400

    yii2yii activeQuery

--- yii2yii/ActiveQuery.php
+++ yii2yii/ActiveQuery.php
@@ -9,6 +9,7 @@
 namespace bariew\yii2Tools\yii2yii;
 
 
+use yii\db\ActiveRecord;
 use yii\db\Query;
 
 class ActiveQuery extends \yii\db\ActiveQuery
@@ -26,26 +27,44 @@ class ActiveQuery extends \yii\db\ActiveQuery
      */
     public function replaceAttributes()
     {
-        $class = $this->modelClass;
-        $model = new $class();
         foreach ($this->replacements as $old => $new) {
             $this->$new = $this->$old;
         }
         if ($this->orderBy) {
             $resultOrders = [];
             $orders = is_array($this->orderBy) ? $this->orderBy : explode(',', $this->orderBy);
-            foreach ($orders as $order) {
+            foreach ($orders as $key => $order) {
+                if (is_numeric($order)) {
+                    $resultOrders[$key] = $order;
+                    continue;
+                }
                 $data = explode(' ', trim($order));
                 $data[1] = (@$data[1] == 'DESC' ? SORT_DESC : SORT_ASC);
                 $resultOrders[$data[0]] = $data[1];
             }
             $this->orderBy = $resultOrders;
         }
+        if ($this->select) {
+            $this->select = is_array($this->select) ? $this->select : explode(',', $this->select);
+            $select = [];
+            foreach ($this->select as $key => $string) {
+                $pattern = '/(as|AS) (\w+)/';
+                if (preg_match($pattern, $string, $matches)) {
+                    $key = $matches[2];
+                    $string = preg_replace($pattern, '', $string);
+                }
+                $select[$key] = $string;
+            }
+            $this->select = $select;
+        }
 
         if ($this->join && !is_array($this->join)) {
             $this->join = [$this->join];
         }
         if ($this->with) {
+            /** @var \yii\db\ActiveRecord $model */
+            $class = $this->modelClass;
+            $model = new $class();
             $with = [];
             foreach ($this->with as $name => $value) {
                 $relName = is_numeric($name) ? $value : $name;
@@ -54,21 +73,18 @@ class ActiveQuery extends \yii\db\ActiveQuery
                 }
                 if (preg_match('/\./', $relName)) {
                     continue;
-                }                $with[$name] = $value;
-                if (!is_array($value)) {
-                    continue;
                 }
-                /** @var ActiveQuery $relation */
-                $relation = call_user_func([$model, "get{$name}"]);
-                if ($relation->via) {
-                    $relation = $relation->via[1];
-                }
-                /** @var ActiveQuery $relationQuery */
-                $with[$name] = function ($relationQuery) use ($relation, $value) {
-                    $newQuery = new ActiveQuery($relation->modelClass, $value);
-                    $newQuery->replaceAttributes();
-                    $newQuery::merge($relationQuery->via[1], $newQuery);
-                };
+                $with[] = $relName;
+                static::replaceTables($value, $model);
+                $this->joinWith(
+                    [$relName => function($query) use ($value) {
+                        return is_array($value)
+                            ? ActiveQuery::merge($query, new ActiveQuery($query->modelClass, $value))
+                            : true;
+                    }],
+                    true,
+                    (@$value['together'] ? 'RIGHT JOIN' : 'LEFT JOIN')
+                );
             }
             $this->with = $with;
         }
@@ -83,7 +99,6 @@ class ActiveQuery extends \yii\db\ActiveQuery
         if (method_exists($model, 'scopes')) {
             foreach ($model->scopes() as $config) {
                 $scope = new static($class, $config);
-                $scope->replaceAttributes();
                 $this->mergeWith($scope);
             }
         }
@@ -98,21 +113,25 @@ class ActiveQuery extends \yii\db\ActiveQuery
         if (is_array($from)) {
             $from = new Query($from);
         }
+        if ($from instanceof self) {
+            $from->replaceAttributes();
+        }
         $class = $to->modelClass;
-        $table = $class::tableName();
+        /** @var \yii\db\ActiveRecord $model */
+        $model = new $class();
         /** @var \yii\db\ActiveQuery $from */
-        $to->andWhere($from->where);
+        if ($from->where) {
+            $to->andWhere($from->where);
+        }
         $to->addParams($from->params);
-        foreach (['orderBy', 'limit', 'with'] as $param) {
+        foreach (['orderBy', 'limit', 'with', 'join', 'on', 'link', 'via', 'joinWith', 'select'] as $param) {
             if (!is_null($from->$param)) {
                 $to->$param = $from->$param;
             }
         }
-        foreach (['join', 'where'] as $param) {
-            if ($to->where) {
-                static::replaceInArray($to->$param, function(&$v) use($table) {
-                    $v = preg_replace('/[\w_]+\./', $table.'.', $v);
-                });
+        foreach ([ 'where', 'orderBy', 'join', 'select'] as $param) {
+            if ($to->$param) {
+                static::replaceTables($to->$param, $model);
             }
         }
 
@@ -124,9 +143,32 @@ class ActiveQuery extends \yii\db\ActiveQuery
         return static::merge($this, $query);
     }
 
+    public static function replaceTables(&$data, ActiveRecord $model)
+    {
+        static::replaceInArray($data, function(&$v) use($model) {
+            if (!is_string($v)) {
+                return;
+            }
+            $v = preg_replace_callback('/(\W?)([\w_]+)\./', function($matches) use($model){
+                try {$relation = @$model->getRelation($matches[2]);}catch (\Exception  $e){ $relation=false;}
+                if ($matches[2] == 't') {
+                    $table = $model::tableName();
+                } elseif ($relation) {
+                    $class = $relation->modelClass;
+                    $table = $class::tableName();
+                } else {
+                    $table = $matches[2];
+                }
+                return $matches[1] . $table.'.';
+            }, $v);
+        });
+    }
+
     public static function replaceInArray(&$data, $function)
     {
-        if (is_array($data)) {
+        if (is_object($data)) {
+            return;
+        } else if (is_array($data)) {
             array_walk_recursive($data, $function);
             static::arrayWalkRecursiveKeys($data, $function);
         } else {
